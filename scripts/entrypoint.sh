cb806d1ecafb3f0b694251feb71ea9ec829f9fdb
Venkata Naga Ravikiran Bulusu <ravi.has.kiran@gmail.com>
2023-10-10 15:20:32 +0000

    cleanup

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,7 +4,6 @@ qemu-system-x86_64 \
     -m 256 \
     -cpu IvyBridge \
     -machine q35 \
-    -device AC97 \
     -kernel bzImage-qemux86-64.bin \
     -drive if=virtio,format=raw,file=core-image-minimal-qemux86-64.rootfs.ext4 \
     -append "root=/dev/hda rw console=ttyS0,115200" \
